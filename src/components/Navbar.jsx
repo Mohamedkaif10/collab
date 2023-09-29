ce86c24bc5ad5ee407701409742e41cde93d80ad
MohamedKaif-ft <141742064+MohamedKaif-ft@users.noreply.github.com>
2023-09-29 19:14:22 +0530

    committing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import "../styles/Navbar.css"
+import ProCollab from "../assets/ProCollab.png"
 const Navbar=()=>{
     return(
         <>
-        <nav>
-            <p>something</p></nav></>
+        <div className="nav_div_main">
+            <img src="ProCollab.png" alt="" className="nav_img" />
+        </div>
+        </>
     )
 }
 export default Navbar
